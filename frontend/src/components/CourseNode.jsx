4e286a91d8faf337de7260709258b9a644976c11
Jaiman Munshi <jaimancoding@gmail.com>
2025-01-19 12:39:52 -0500

    removing old code

--- frontend/src/components/CourseNode.jsx
+++
@@ -1,7 +0,0 @@
-export const CourseNode = ({ data }) => {
-  return (
-    <div className={`course-node ${data.completed ? 'completed' : 'planned'}`}>
-      <div className="course-id">{data.id}</div>
-    </div>
-  );
-};
